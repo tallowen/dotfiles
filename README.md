1059359d0316abb34a3860f9523a4b53df169c2e
Owen Coutts <owen@owencoutts.com>
2013-02-16 16:59:06 -0800

    Add ssh config. (Changes needed for gitignore)
    Added vim as editor

---
+++ README.md
@@ -0,0 +1,4 @@
+Dotfiles
+========
+
+A saner way to try to keep all my dotfiles in sync. Thanks [Dave Dash](https://github.com/davedash/) for the idea!
--- install.sh
+++ install.sh
@@ -6,5 +6,12 @@ for f in * .*; do
     if [ "$f" = "install.sh" ]; then
 	continue
     fi
+    # Don't copy the .gitignore for this repo
+    if [ "$f" = ".gitignore" ]; then
+	continue
+    fi
+    if [ "$f" = "README" ]; then
+	continue
+    fi
     ln -s ~/.dotfiles/$f ~/$f
 done
